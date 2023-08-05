e0128697f1d3f81f3db887d0821cdf80721b12de
Sneha <snehu6369@gmail.com>
2023-08-05 23:49:50 +0530

    videoo

--- app/src/main/java/com/example/videocallingapp/Loginpage.java
+++ app/src/main/java/com/example/videocallingapp/Loginpage.java
@@ -1,14 +1,35 @@
-package com.example.videocallingapp;
+  package com.example.videocallingapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.QuickContactBadge;
 
-public class Loginpage extends AppCompatActivity {
+  public class Loginpage extends AppCompatActivity {
+
+    EditText email,password;
+    Button login,signup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loginpage);
+
+        email=findViewById(R.id.editTextTextEmailAddress);
+        password=findViewById(R.id.editTextTextPassword);
+        login=findViewById(R.id.login);
+        signup=findViewById(R.id.create);
+
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i =new Intent(Loginpage.this,signup.class);
+                startActivity(i);
+            }
+        });
     }
 }
